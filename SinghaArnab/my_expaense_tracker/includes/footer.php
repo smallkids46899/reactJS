a841785b8475156fbc5694a790dff91d397e2e92
smallkids46899 <smallkids46899@protonmail.com>
2024-05-14 23:35:24 +0300

    refactor components

---
+++ SinghaArnab/my_expaense_tracker/includes/footer.php
@@ -0,0 +1,9 @@
+<div class="container-fluid d-flex justify-content-center position-absolute bottom-0 bg-dark text-white">
+		<h6 class="text-center p-2">THANK YOU FOR YOUR BUSINESS</h6>
+            </p>
+            <p class="text-center p-2">
+               <h6 class="p-2"><i class="fa fa-fw fa-lg fa-globe"></i> ExpenseTracker.com</h6>
+               <h6 class="text-center p-2"><i class="fa fa-fw fa-lg fa-phone-volume"></i> T:016-18192302</h6>
+			   <h6 class="text-center p-2"><i class="fa fa-fw fa-lg fa-envelope"></i>uic.21mca2445@gmail.com</h6>
+            </p>
+         </div>
